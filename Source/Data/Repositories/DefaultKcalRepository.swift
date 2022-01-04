1e0c8eebfeda744303ef0335e00b6324762bc8df
baegteun <baegteun@gmail.com>
2022-01-04 17:44:05 +0900

    :zap: :: DefaultKcalRepository

--- Source/Data/Repositories/DefaultKcalRepository.swift
+++ Source/Data/Repositories/DefaultKcalRepository.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2022 baegteun. All rights reserved.
 //
 
-import Foundation
+import RealmSwift
+
+final class DefaultKcalRepository: KcalRepository{
+    private let realm = try! Realm()
+    
+    func setKcal(kcal: Int) {
+        let model = Kcal(kcal: kcal)
+        
+        try? realm.write {
+            realm.add(model)
+        }
+    }
+    
+    func getKcal() -> Int {
+        let kcal = realm.objects(Kcal.self).first ?? .init()
+        return kcal.kcal
+    }
+    
+    
+}
--- Source/Domain/Interfaces/Repositories/KcalRepository.swift
+++ Source/Domain/Interfaces/Repositories/KcalRepository.swift
@@ -7,6 +7,6 @@
 //
 
 protocol KcalRepository{
-    func setKcal()
+    func setKcal(kcal: Int)
     func getKcal() -> Int
 }
