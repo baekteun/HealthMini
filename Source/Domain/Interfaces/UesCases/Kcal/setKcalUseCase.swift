e35aa8b6c3f9d1f328179be63dee0b8991f9e803
baegteun <baegteun@gmail.com>
2022-01-04 17:46:28 +0900

    :zap: :: kcalUseCase

---
+++ Source/Domain/Interfaces/UesCases/Kcal/setKcalUseCase.swift
@@ -0,0 +1,11 @@
+//
+//  setKcalUseCase.swift
+//  HealthMini
+//
+//  Created by 최형우 on 2022/01/04.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+protocol setKcalUseCase{
+    func execute(kcal: Int)
+}
