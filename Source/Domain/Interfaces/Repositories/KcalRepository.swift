da2035d34a817690939073dae37f944576496634
baegteun <baegteun@gmail.com>
2022-01-04 17:39:23 +0900

    :zap: :: KcalRepository

---
+++ Source/Domain/Interfaces/Repositories/KcalRepository.swift
@@ -0,0 +1,12 @@
+//
+//  KcalRepository.swift
+//  HealthMini
+//
+//  Created by 최형우 on 2022/01/04.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+protocol KcalRepository{
+    func setKcal()
+    func getKcal() -> Int
+}
