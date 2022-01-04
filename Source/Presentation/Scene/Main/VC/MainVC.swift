6e7b776331094d75a3d882a5d330f03e9ba439b0
baegteun <baegteun@gmail.com>
2022-01-04 18:28:50 +0900

    :zap: :: MainVC
    create BarChartView

--- Source/Presentation/Scene/Main/VC/MainVC.swift
+++ Source/Presentation/Scene/Main/VC/MainVC.swift
@@ -7,21 +7,36 @@
 //
 
 import UIKit
-import RealmSwift
+import Charts
 
 final class MainVC: baseVC<baseViewModel>{
     // MARK: - Properties
-    
+    private let chart: BarChartView = {
+       let chart = BarChartView()
+        chart.backgroundColor = UIColor(red: 0.2556, green: 0.2556, blue: 0.2556, alpha: 1)
+        chart.layer.cornerRadius = 22
+        chart.clipsToBounds = true
+        chart.noDataText = "아직 데이터가 없습니다!"
+        chart.noDataTextColor = .white
+        chart.noDataFont = UIFont(font: HealthMiniFontFamily.Roboto.bold, size: 24) ?? .init()
+        return chart
+    }()
     
     // MARK: - UI
     override func addView() {
-        
+        view.addSubViews(chart)
     }
     override func setLayout() {
-        
+        NSLayoutConstraint.activate([
+            chart.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            chart.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            chart.widthAnchor.constraint(equalToConstant: bound.width*0.8933),
+            chart.heightAnchor.constraint(equalToConstant: bound.width*0.8933 - 20)
+        ])
     }
     override func configureVC() {
-        view.backgroundColor = .green
-        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.title = "Today Exercise"
+        self.navigationItem.accessibilityLabel = "Today Exercise"
     }
 }
