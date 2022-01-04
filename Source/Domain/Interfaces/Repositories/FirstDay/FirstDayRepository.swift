7a5888da32ee085a1666fffb1f13c9bc4de7a909
baegteun <baegteun@gmail.com>
2022-01-04 22:34:24 +0900

    :zap: :: Repository FirstDayRepository
    create firstDayRepository

---
+++ Source/Domain/Interfaces/Repositories/FirstDay/FirstDayRepository.swift
@@ -0,0 +1,14 @@
+//
+//  FirstDayRepository.swift
+//  HealthMini
+//
+//  Created by 최형우 on 2022/01/04.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import Foundation
+
+protocol FirstDayRepository{
+    func setFirstDay()
+    func getFirstDay() -> Date
+}
