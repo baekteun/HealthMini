323e2c57757e8d11a1d3e1b7aeb9bb2474607e72
baegteun <baegteun@gmail.com>
2022-01-04 22:41:59 +0900

    :zap: :: UseCase
    create SetStartDayUseCase

---
+++ Source/Domain/Interfaces/UesCases/StartDay/SetStartDayUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  SetStartDayUseCase.swift
+//  HealthMini
+//
+//  Created by 최형우 on 2022/01/04.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import Foundation
+
+protocol SetStartDayUseCase{
+    func execute(date: Date)
+}
