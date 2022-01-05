81dcbc53fe8d29baf4728254f46cfdbf7cbe52e0
baegteun <baegteun@gmail.com>
2022-01-05 12:58:44 +0900

    :zap: :: WalkCountCell

--- Source/Presentation/Scene/Main/Walking/View/Cell/WalkCountCell.swift
+++ Source/Presentation/Scene/Main/Walking/View/Cell/WalkCountCell.swift
@@ -8,4 +8,40 @@
 
 import UIKit
 
-
+final class WalkCountCell: BaseTableViewCell<StepWithDay>{
+    // MARK: - Properties
+    private let stepLabel: UILabel = {
+        let lb = UILabel()
+        lb.font = UIFont(font: HealthMiniFontFamily.Roboto.bold, size: 18)
+        return lb
+    }()
+    
+    private let dateLabel: UILabel = {
+        let lb = UILabel()
+        lb.font = UIFont(font: HealthMiniFontFamily.Roboto.bold, size: 12)
+        lb.textAlignment = .right
+        return lb
+    }()
+    
+    // MARK: - UI
+    override func addView() {
+        addSubViews(stepLabel, dateLabel)
+    }
+    override func setLayout() {
+        NSLayoutConstraint.activate([
+            stepLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            stepLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            
+            dateLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            dateLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
+        ])
+    }
+    override func configureCell() {
+        self.layer.cornerRadius = 20
+        self.addShadow(color: .black, radius: 2, offSet: CGSize(width: 0, height: 1), opacity: 1)
+    }
+    
+    override func bind(_ model: StepWithDay) {
+        
+    }
+}
