dea435d7b0b8807e6a0a9c8432886a7c45a8434c
baegteun <baegteun@gmail.com>
2022-01-05 13:16:45 +0900

    :zap: :: HealthRepository

---
+++ Source/Domain/Interfaces/Repositories/Health/HealthRepository.swift
@@ -0,0 +1,11 @@
+//
+//  HealthRepository.swift
+//  HealthMini
+//
+//  Created by 최형우 on 2022/01/05.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+protocol HealthRepository{
+    func getAllStepWithDay()
+}
