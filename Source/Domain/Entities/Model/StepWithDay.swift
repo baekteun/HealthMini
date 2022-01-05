848a88b531cc8722a1b2c9ddc0f8be6029d57848
baegteun <baegteun@gmail.com>
2022-01-05 12:29:05 +0900

    :zap: :: Entity
    create StepWithDay

---
+++ Source/Domain/Entities/Model/StepWithDay.swift
@@ -0,0 +1,14 @@
+//
+//  StepWithDay.swift
+//  HealthMini
+//
+//  Created by 최형우 on 2022/01/05.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import Foundation
+
+struct StepWithDay{
+    let date: Date
+    let stepCount: Int
+}
